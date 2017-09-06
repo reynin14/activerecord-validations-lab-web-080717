6492a7b1236a22ea7dea6d610fb3bad7fa259ab2
github username <github email address>
2017-09-06 13:37:51 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category"}
+  validate :is_click_bait
+
+  def is_click_bait
+    if !self.title.nil? && self.title.include?("Won't Believe" ||  "Secret" || "Top" || "Guess")
+    else
+      errors.add(:title, "Not click bait!")
+    end
+  end
+  
 end
